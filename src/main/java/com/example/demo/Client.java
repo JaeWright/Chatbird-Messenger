b97299b7b9a6b27ce712cc0517a2ec289f6c8b26
Amit Sarvate <amit@sarvate.com>
2022-04-13 23:01:35 -0400

    revamped fxmls

---
+++ src/main/java/com/example/demo/Client.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Client {
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private TextField messageInput;
+
+    @FXML
+    private ListView<?> messagesBox;
+
+    @FXML
+    private Label receiverLabel;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    void exitBtnOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendBtnOnClick(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/Server.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class Server {
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private ListView<?> serverLogBox;
+
+    @FXML
+    void exitBtnOnClick(ActionEvent event) {
+
+    }
+
+}
